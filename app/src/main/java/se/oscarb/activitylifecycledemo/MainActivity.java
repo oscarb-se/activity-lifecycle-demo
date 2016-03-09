fbf779397cfff3ecc083a78c39e2ed0265222ab6
Oscar <github@oscarb.se>
2016-03-09 10:45:41 +0100

    Added instructions for Activity Lifecycle excercise

--- app/src/main/java/se/oscarb/activitylifecycledemo/MainActivity.java
+++ app/src/main/java/se/oscarb/activitylifecycledemo/MainActivity.java
@@ -2,13 +2,24 @@ package se.oscarb.activitylifecycledemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+        När och i vilken ordning körs de olika metoderna?
+        Pröva olika saker i mobilen! Kan du få alla att köras?
+        Anteckna vad du gjorde som fick respektive metod att köras
+        och vilka metoder som kördes precis innan/efter.
+
+        Fundera också på - vilka tillstånd gick appen mellan?
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Activity Lifecycle", "Metoden onCreate() körs!");
     }
 
     @Override
