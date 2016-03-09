6212287ab6f349d25ac8d1c6d8837aba3ccd3182
VoidTech <wojciech.wysocki96@gmail.com>
2016-03-09 11:30:24 +0100

    Added comments

--- app/src/main/java/se/oscarb/activitylifecycledemo/MainActivity.java
+++ app/src/main/java/se/oscarb/activitylifecycledemo/MainActivity.java
@@ -22,31 +22,42 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Activity Lifecycle", "Metoden onCreate() körs!");
     }
 
+    /* 
+    Den här metoden körs när man klickar på "Bakåt" knappen och att Activty hhar varit uppe i länge, men innan onDestoy() körs också onStop()  /Wojtek
+    */
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
 
+    // Metoden onPause() körs när man klickar "bakåt" knappen kort efter Activty har startat /Wojtek
+
     @Override
     protected void onPause() {
         super.onPause();
     }
-
+    
+    //Metoden onRestart() körs när man återupptar Activty efter ett längre period /Wojtek
+    
     @Override
     protected void onRestart() {
         super.onRestart();
     }
 
+    //Metoden onResume() körs efter onCreate() och onStart() och när man återupptar Activity efter en kort period /Wojtek
+    
     @Override
     protected void onResume() {
         super.onResume();
     }
 
+    //Den metoden körs efter onCreate() Activty har skappats /Wojtek
     @Override
     protected void onStart() {
         super.onStart();
     }
-
+    
+    //Metoden onStop() körs när man klickar "hemma" knappen  /Wojtek
     @Override
     protected void onStop() {
         super.onStop();
